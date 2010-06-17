b0ed178ed907f01611e10ea058a021cafd3e532e
Handol Park <dolhana@dolmbp.local>
2010-06-16 19:32:03 -0700

    Compiles against Scala 2.8.0.RC5.

---
+++ src/main/java/me/handol/scala/HelloScala.java
@@ -0,0 +1,9 @@
+package me.handol.scala;
+
+public class HelloScala {
+    public static void helloScala() {
+	System.out.println("Hello from Java");
+	// call scala class
+	//System.out.println("Scala says: " + App.helloJava());
+    }
+}
--- src/main/scala/me/handol/scala/App.scala
+++ src/main/scala/me/handol/scala/App.scala
@@ -4,6 +4,9 @@ package me.handol.scala
  * Hello world!
  *
  */
-object App extends Application {
-  println( "Hello World!" )
+object App {
+  // call java class
+  def main(args: Array[String]) = HelloScala.helloScala
+
+  def helloJava(): String = "Hello from Scala!"
 }
